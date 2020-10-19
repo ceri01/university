5429304c12abcde18d710f80d46cf35b6bc8bfbe
Daniele Ceribelli <danieleceribelli@gmail.com>
2020-10-19 21:11:28 +0200

    add homeworks (2020-10-19 and 2020-10-18)

---
+++ 2020-10-19/MCD.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, c int
+	fmt.Println("Inserisci un numero")
+	fmt.Scan(&n)
+	if n==0 {
+		c = 1
+	} else {
+		for n != 0{
+			n /= 10
+			c++
+		}
+	}
+	fmt.Println("Numero di cifre:", c)
+}
