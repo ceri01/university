dc5029da5aa4395a42a62788a97cdb1f3a6df531
Daniele Ceribelli <danieleceribelli@gmail.com>
2020-10-21 09:02:38 +0200

    Code optimization

--- 2020-10-19/potenze_di_2.go
+++ 2020-10-19/potenze_di_2.go
@@ -8,16 +8,11 @@ func main() {
 	var n int
 	fmt.Println("Inserisci un numero")
 	fmt.Scan(&n)
-	potenza := 2
-	i := 1
-	if n >= 2 {
-		fmt.Println("2 alla", i, "= ", potenza)
-	}
-	for potenza < n {
+	potenza := 1
+	i := 0
+	for potenza <= n {
+		fmt.Println("1 alla", i, "= ", potenza)
 		i++
 		potenza *= 2
-		if potenza <= n {
-			fmt.Println("2 alla", i, "= ", potenza)
-		}
 	}
 }
