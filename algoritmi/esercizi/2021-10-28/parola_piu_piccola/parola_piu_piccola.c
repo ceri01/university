e3a631ce3fde3a11b12269edce36543727ef93c3
ceri01 <danieleceribelli@gmail.com>
2021-11-03 11:48:05 +0100

    Fix parola_piu_piccola.c

--- algoritmi/esercizi/2021-10-28/parola_piu_piccola/parola_piu_piccola.c
+++ algoritmi/esercizi/2021-10-28/parola_piu_piccola/parola_piu_piccola.c
@@ -5,13 +5,13 @@ typedef char* String;
 
 int smallest_word_index(char *s[], int n);
 
-String *smallest_word_indexx(String s[], int n);
+String *smallest_word_address(String s[], int n);
 
 int main( void ) {
     String dict[] = { "ciao", "mondo", "come", "funziona", "bene", "il", "programma" };
     int lun = 7;
     printf("%d\n", smallest_word_index(dict, lun));
-    printf("%s\n", *smallest_word_indexx(dict, lun));
+    printf("%s\n", *smallest_word_address(dict, lun));
 }
 
 int smallest_word_index(String s[], int n) {
@@ -28,15 +28,17 @@ int smallest_word_index(String s[], int n) {
     return index;
 }
 
-String *smallest_word_indexx(String s[], int n) {
-    String min = s[0];
+char **smallest_word_address(char * s[], int n) {
+    int min_pos = 0;
 
     for(int i = 1; i < n; i++) {
-        if(strcmp(min, s[i]) > 0) {
-            min = s[i];
+        if(strcmp(s[min_pos], s[i]) > 0) {
+            min_pos = i;
         }
-    }
-    printf("%s", min);
-    printf("%p", &min);   
-    return min;
+    } 
+    return s + min_pos;
+    // String s[] = **s = puntatore --> (array di puntatori in cui ogni puntatore punta a un array di caratteri) puntatore --> primo carattere della stringa puntata
+    // s = il suo valore e' l'indirizzo del puntatore che punta al primo array di caratteri
+    // min_pos posizione della stringa piu' piccola
+    // s + min_pos = indirizzo del puntatore che punta alla stringa pi' piccola
 }
