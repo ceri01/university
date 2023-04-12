6f5294c0db949b7f74e45e6e60b5fc9aca959f95
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-04-12 11:59:34 +0200

    Add bubble sort

--- algoritmi/algoritmi_implementati/main.go
+++ algoritmi/algoritmi_implementati/main.go
@@ -8,8 +8,10 @@ import (
 func main() {
 	arrSelection := []int{3, 6, 1, 0, 22, 4, 3}
 	arrInsertion := []int{25, 16, 2, 67, 25, 19, 6}
+	arrBubble := []int{47, 12, 12, 7, 25, 1, 65}
 	ordinamento.Selection_sort(arrSelection)
 	ordinamento.Insertion_sort(arrInsertion)
+	ordinamento.Bubble_sort(arrBubble)
 
 	fmt.Println("Array ordinato con Selection sort")
 	for _, el := range arrSelection {
@@ -18,6 +20,11 @@ func main() {
 	fmt.Println("")
 	fmt.Println("Array ordinato con Insertion sort")
 	for _, el := range arrInsertion {
+		fmt.Printf("%d ", el)
+	}
+	fmt.Println("")
+	fmt.Println("Array ordinato con Bubble sort")
+	for _, el := range arrBubble {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
--- algoritmi/algoritmi_implementati/ordinamento/tecniche_base/bubble_sort.go
+++ algoritmi/algoritmi_implementati/ordinamento/tecniche_base/bubble_sort.go
@@ -1,5 +1,19 @@
 package ordinamento
 
-func Bubble_sort() {
+import "algoritmi/algoritmi/algoritmi_implementati/util"
 
+func Bubble_sort(arr []int) {
+	i := 1
+	dim := len(arr)
+	swtch := true
+	for swtch && i < dim {
+		swtch = false
+		for j := 1; j < dim-i; j++ { // oppure i <= dim - 1
+			if arr[j] < arr[j-1] {
+				util.Swap(arr, j-1, j)
+				swtch = true
+			}
+		}
+		i++
+	}
 }
