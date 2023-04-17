265738258e6d121605c3f198644c9d9b4ff8545b
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-04-17 10:39:38 +0200

    Add merge sort

--- algoritmi/algoritmi_implementati/main.go
+++ algoritmi/algoritmi_implementati/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	ordinamento2 "algoritmi/algoritmi/algoritmi_implementati/ordinamento/tecniche_avanzate"
 	ordinamento "algoritmi/algoritmi/algoritmi_implementati/ordinamento/tecniche_base"
 	"fmt"
 )
@@ -9,9 +10,11 @@ func main() {
 	arrSelection := []int{3, 6, 1, 0, 22, 4, 3}
 	arrInsertion := []int{25, 16, 2, 67, 25, 19, 6}
 	arrBubble := []int{47, 12, 12, 7, 25, 1, 65}
+	arrMerge := []int{16, 3, 45, 22, 252, 56, 5}
 	ordinamento.Selection_sort(arrSelection)
 	ordinamento.Insertion_sort(arrInsertion)
 	ordinamento.Bubble_sort(arrBubble)
+	ordinamento2.Merge_sort(arrMerge)
 
 	fmt.Println("Array ordinato con Selection sort")
 	for _, el := range arrSelection {
@@ -25,6 +28,12 @@ func main() {
 	fmt.Println("")
 	fmt.Println("Array ordinato con Bubble sort")
 	for _, el := range arrBubble {
+		fmt.Printf("%d ", el)
+	}
+	fmt.Println("")
+
+	fmt.Println("Array ordinato con Merge sort")
+	for _, el := range arrMerge {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
--- algoritmi/algoritmi_implementati/ordinamento/tecniche_avanzate/merge_sort.go
+++ algoritmi/algoritmi_implementati/ordinamento/tecniche_avanzate/merge_sort.go
@@ -1,5 +1,51 @@
 package ordinamento
 
-func Merge_sort() {
+func Merge_sort(arr []int) { // versione già migliorata
+	// Costo tempo O(n*log(n))
+	// Costo tempo O(log(n))
+	dim := len(arr)
+	arr2 := make([]int, dim)
+	mergeSort(arr, 0, dim, arr2)
+}
 
+func mergeSort(arr []int, i int, f int, arr2 []int) {
+	if f-i > 1 { // costo spazio O(log(n)), le due chiamate ricorsive occupano lo stesso spazio,
+		// quando una chiamata termina la successiva occupa lo spazio gi' usato dall'altro record di attivazione
+		m := (i + f) / 2
+		mergeSort(arr, i, m, arr2)
+		mergeSort(arr, m, f, arr2)
+		merge(arr, i, m, f, arr2)
+	}
+}
+
+func merge(arr []int, i int, m int, f int, arr2 []int) {
+	i1 := i
+	i2 := m
+	k := 0
+
+	for i1 < m && i2 < f { // Costo tempo O(n*log(n))
+		if arr[i1] <= arr[i2] {
+			arr2[k] = arr[i1]
+			i1++
+		} else {
+			arr2[k] = arr[i2]
+			i2++
+		}
+		k++
+	}
+	if i1 < m { // Costo tempo O(1)
+		for ; i1 < m; i1++ {
+			arr2[k] = arr[i1]
+			k++
+		}
+	} else { // costo O(1)
+		for ; i2 < f; i2++ {
+			arr2[k] = arr[i2]
+			k++
+		}
+	}
+
+	for k := 0; k < f-i; k++ { // Costo tempo O(n)
+		arr[i+k] = arr2[k]
+	}
 }
