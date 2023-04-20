94a66adcc899374b8094ba9aab4a7931163f3eec
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-04-20 09:02:34 +0200

    Add quick sort

--- algoritmi/algoritmi_implementati/main.go
+++ algoritmi/algoritmi_implementati/main.go
@@ -11,29 +11,38 @@ func main() {
 	arrInsertion := []int{25, 16, 2, 67, 25, 19, 6}
 	arrBubble := []int{47, 12, 12, 7, 25, 1, 65}
 	arrMerge := []int{16, 3, 45, 22, 252, 56, 5}
+	arrQuick := []int{123, 431, 84, 732, 22, 1, -3, 32}
+
 	ordinamento.Selection_sort(arrSelection)
 	ordinamento.Insertion_sort(arrInsertion)
 	ordinamento.Bubble_sort(arrBubble)
 	ordinamento2.Merge_sort(arrMerge)
+	ordinamento2.Quick_sort(arrQuick)
 
-	fmt.Println("Array ordinato con Selection sort")
+	fmt.Println("Array ordinato con Selectionsort")
 	for _, el := range arrSelection {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
-	fmt.Println("Array ordinato con Insertion sort")
+	fmt.Println("Array ordinato con Insertionsort")
 	for _, el := range arrInsertion {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
-	fmt.Println("Array ordinato con Bubble sort")
+	fmt.Println("Array ordinato con Bubblesort")
 	for _, el := range arrBubble {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
 
-	fmt.Println("Array ordinato con Merge sort")
+	fmt.Println("Array ordinato con Mergesort")
 	for _, el := range arrMerge {
+		fmt.Printf("%d ", el)
+	}
+	fmt.Println("")
+
+	fmt.Println("Array ordinato con Quicksort")
+	for _, el := range arrQuick {
 		fmt.Printf("%d ", el)
 	}
 	fmt.Println("")
--- algoritmi/algoritmi_implementati/ordinamento/tecniche_avanzate/quick_sort.go
+++ algoritmi/algoritmi_implementati/ordinamento/tecniche_avanzate/quick_sort.go
@@ -1,5 +1,44 @@
 package ordinamento
 
-func Quick_sort() {
+import "algoritmi/algoritmi/algoritmi_implementati/util"
 
+func partitioning(arr []int, i int, f int) int {
+	pivot := arr[i]
+	sx := i
+	dx := f
+	for sx < dx {
+		for arr[dx] > pivot {
+			dx--
+		}
+		for sx < dx && arr[sx] <= pivot {
+			sx++
+		}
+		if sx < dx {
+			util.Swap(arr, sx, dx)
+		}
+	}
+	util.Swap(arr, i, dx)
+	return dx
+}
+
+// Versione senza ricorsione in coda (altezza stack costante)
+// Inoltre questa versione riordina la partizione di array più piccola in modo da
+// poter avere i record di attivazione più piccoli (la metà rispetto alla lunghezza dell'array originale)
+
+func quickSort(arr []int, i int, f int) {
+	for f-i > 1 {
+		pivot := partitioning(arr, i, f)
+		if pivot-i < f-pivot {
+			quickSort(arr, i, pivot)
+			i = pivot + 1
+		} else {
+			quickSort(arr, pivot+1, f)
+			f = pivot
+		}
+	}
+}
+
+func Quick_sort(arr []int) {
+	dim := len(arr) - 1
+	quickSort(arr, 0, dim)
 }
