e633d12b535ca3ae43fe2211600aca642079883d
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-05-17 17:48:40 +0200

    Add function to get slice of keys from slice of records

--- algoritmi/implementedAlgorithms/sort/util.go
+++ algoritmi/implementedAlgorithms/sort/util.go
@@ -4,3 +4,11 @@ type Record struct {
 	Key  int
 	Data string
 }
+
+func Keys(data []*Record) *[]int {
+	var keys []int
+	for _, el := range data {
+		keys = append(keys, el.Key)
+	}
+	return &keys
+}
